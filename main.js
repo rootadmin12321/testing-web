64af6381c5df268287407a7743bd6a1270c47555
rootadmin12321 <rp.rohit0099@gmail.com>
2025-04-04 09:10:32 +0530

    gnfdbh

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
-    return res.json({ message: 'Hello, I am a Node.js container' });
+    return res.json({ message: 'Hello, I am a Node.js container V1.0' });
 });
 
 app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`));
